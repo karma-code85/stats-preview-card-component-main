4596f20b9e4d6e33c6bcb0515b688b439734f510
Marwan Mohamed <mac@192.168.1.81>
2026-04-08 19:13:47 -0700

    whole project

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,14 +2,14 @@ export default function App(){
   return (
     <div className="min-h-screen flex justify-center items-center  bg-black p-4 relative">
       <div className="absolute inset-0 bg-[hsl(277,64%,61%)] opacity-20"></div>
-      <div className="flex flex-col rounded-lg bg-[hsl(244,37%,16%)] space-y-6  shadow-lgrm -rf my-project/.git" >
-        <div className="w-full h-full  ">
-          <img src="./images/image-header-mobile.jpg" alt=""  className="rounded-tl-lg rounded-tr-lg"/>
+      <div className=" md:flex rounded-lg bg-[hsl(244,37%,16%)] space-y-6  shadow-lgrm  max-w-6xl" >
+        <div className="w-full h-full  md:w-1/2 md:order-2">
+          <img src="./images/image-header-mobile.jpg" alt=""  className="rounded-tl-lg rounded-tr-lg md:rounded-tr-lg md:rounded-br-lg md:rounded-tl-none"/>
         </div>
-        <div className="p-4 space-y-4">
-          <h1 className="font-semibold text-center text-white text-3xl">Get <span className="text-[hsl(277,64%,61%)]">insights</span> that help your business grow. </h1>
-          <p className="text-center text-white">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. </p>
-          <div className="text-center space-y-4 text-white">
+        <div className="p-4 space-y-4 md:w-1/2 md:order-1 md:p-10 md:leading-9 md:text-lg">
+          <h1 className="font-semibold text-center md:text-start text-white text-3xl md:font-extrabold md:text-4xl leading-2">Get <span className="text-[hsl(277,64%,61%)]">insights</span> that help your business grow. </h1>
+          <p className="text-center md:text-start text-white">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. </p>
+          <div className="text-center space-y-4 text-white md:flex items-center justify-center md:justify-between md:items-center">
             <div>
               <h1 className="font-extrabold text-xl">10k+ </h1>
               <p>companies</p>
